b244f07935442f6653f31b8581dc724b41f0a86b
DevExpressExampleBot <fakeEmail>
2022-12-17 08:44:43 +0400

    Source auto update [skip ci]

--- VB/WpfApplication303/Views/OptionsView.xaml.vb
+++ VB/WpfApplication303/Views/OptionsView.xaml.vb
@@ -1,27 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication303.Views
-	''' <summary>
-	''' Interaction logic for OptionsView.xaml
-	''' </summary>
-	Partial Public Class OptionsView
-		Inherits UserControl
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for OptionsView.xaml
+    ''' </summary>
+    Public Partial Class OptionsView
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
--- VB/WpfApplication303/Views/ToolsView.xaml.vb
+++ VB/WpfApplication303/Views/ToolsView.xaml.vb
@@ -1,27 +1,15 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace WpfApplication303.Views
-	''' <summary>
-	''' Interaction logic for ToolsView.xaml
-	''' </summary>
-	Partial Public Class ToolsView
-		Inherits UserControl
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for ToolsView.xaml
+    ''' </summary>
+    Public Partial Class ToolsView
+        Inherits UserControl
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
